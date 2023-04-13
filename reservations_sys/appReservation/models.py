3c90c391aa0b74d85cc585ba48396fa44e80c9cf
Amir Shamsi <emirshamsi.uni@gmail.com>
2023-04-13 16:50:13 +0330

    necessary models were added

--- reservations_sys/appReservation/models.py
+++ reservations_sys/appReservation/models.py
@@ -1,1 +1,29 @@
+from django.db import models
 
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=50)
+    room_number = models.CharField(max_length=10)
+    price_per_night = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.listing.name} - {self.room_type}"
+
+
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} - {self.room.listing.name} - {self.room.room_type}"
