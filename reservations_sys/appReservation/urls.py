146a449f1ceeeab2d1fb0d3260a2fbb3ed733b80
Amir Shamsi <emirshamsi.uni@gmail.com>
2023-04-15 17:06:27 +0330

    create room API to get available or all rooms list

--- reservations_sys/appReservation/urls.py
+++ reservations_sys/appReservation/urls.py
@@ -5,6 +5,9 @@ from . import views
 # Create a router for the ListingViewSet
 router = routers.DefaultRouter()
 router.register(r'listings', views.ListingViewSet)
+# Create a nested router for the RoomViewSet, nested under the ListingViewSet
+listings_router = routers.NestedDefaultRouter(router, r'listings', lookup='listing')
+listings_router.register(r'rooms', views.RoomViewSet, basename='listing-rooms')
 urlpatterns = [
     # Include the urls for the ListingViewSet
     path('', include(router.urls)),
--- reservations_sys/appReservation/views.py
+++ reservations_sys/appReservation/views.py
@@ -18,3 +18,47 @@ class ListingViewSet(viewsets.ModelViewSet):
     serializer_class = ListingSerializer
     pagination_class = Pagination
 
+
+# ViewSet to get Rooms
+class RoomViewSet(viewsets.ModelViewSet):
+    pagination_class = Pagination
+
+    def get_queryset(self):
+        # Get the listing ID from the URL, if it exists
+        listing_pk = self.kwargs.get('listing_pk')
+        if listing_pk:
+            queryset = Room.objects.filter(listing_id=listing_pk)
+        else:
+            queryset = Room.objects.all()
+
+        return get_list_or_404(queryset)
+
+    def create(self, request, *args, **kwargs):
+        # Validate input data and get start_time and end_time
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        start_time = serializer.validated_data['start_time']
+        end_time = serializer.validated_data['end_time']
+
+        # Get the list of available rooms for the given time range
+        queryset = Room.objects.filter(~Q(reservation__start_time__lt=end_time, reservation__end_time__gt=start_time))
+
+        # If a listing ID was provided in the URL, filter the queryset by that ID
+        listing_pk = self.kwargs.get('listing_pk')
+        if listing_pk:
+            queryset = queryset.filter(listing_id=self.kwargs['listing_pk'])
+
+        # Serialize the queryset and return the response
+        serializer = RoomSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        # Return a different serializer class depending on the request method
+        if self.request.method == 'GET':
+            return RoomSerializer
+        elif self.request.method == 'POST':
+            return RoomAvailabilitySerializer
+        else:
+            return super().get_serializer_class()
+
